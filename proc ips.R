6d0d33214414f2cb1ba6911bd8e35359e7299433
Michael Herradora <mherradora@gmail.com>
2016-06-07 00:45:16 -0600

    Inclusion de nuevos elementos en la visualizacion.

--- proc ips.R
+++ proc ips.R
@@ -3,7 +3,15 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 require(plotly)
+require(readr)
 
 ips <- read.csv("~/MHQ/Dropbox/ToGitHub/IPS-cantonal/inputs/cr-IPS-cantonal.csv", stringsAsFactors=FALSE)
 
-plot_ly(ips, y = Agua.y.Saneamiento, color = as.factor(Provincia), type = "box")
+cantones_2011 <- read_csv("~/MHQ/Dropbox/ToGitHub/IPS-cantonal/inputs/cantones_censo2011_demograficos.csv")
+
+ips_ampliado <- ips %>%
+  merge(cantones_2011, by.x = "Cve", by.y = "No.Unidad", all = T)
+
+saveRDS(ips_ampliado,"ips_ampliado.Rds")
+
+# plot_ly(ips, y = Agua.y.Saneamiento, color = as.factor(Provincia), type = "box")
