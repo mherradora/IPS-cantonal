f0b774f41cc0ca697734ec0cb5b271fe234710ab
Michael Herradora <mherradora@gmail.com>
2016-06-09 22:47:57 -0600

    Revisión de la composición de los indices y subindices.

--- proc ips.R
+++ proc ips.R
@@ -5,9 +5,9 @@ require(ggplot2)
 require(plotly)
 require(readr)
 
-ips <- read.csv("~/MHQ/Dropbox/ToGitHub/IPS-cantonal/inputs/cr-IPS-cantonal.csv", stringsAsFactors=FALSE)
+ips <- read.csv("./inputs/cr-IPS-cantonal.csv", stringsAsFactors=FALSE)
 
-cantones_2011 <- read_csv("~/MHQ/Dropbox/ToGitHub/IPS-cantonal/inputs/cantones_censo2011_demograficos.csv")
+cantones_2011 <- read_csv("./inputs/cantones_censo2011_demograficos.csv")
 
 ips_ampliado <- ips %>%
   merge(cantones_2011, by.x = "Cve", by.y = "No.Unidad", all = T)
@@ -15,3 +15,79 @@ ips_ampliado <- ips %>%
 saveRDS(ips_ampliado,"ips_ampliado.Rds")
 
 # plot_ly(ips, y = Agua.y.Saneamiento, color = as.factor(Provincia), type = "box")
+
+# Necesidades humanas básicas ----
+# Nutrición y Asistencia Médica Básica
+## Tasa de mortalidad infantil
+## Muertes por enfermedades infecciosas
+## Intensidad del déficit alimentario
+## Tasa de mortalidad materna
+
+# Agua y Saneamiento
+## Acceso a fuentes de abastecimiento de agua potable mejoradas
+## Acceso a saneamiento mejorado
+
+# Vivienda
+## Hacinamiento
+## Calidad de la vivienda
+## Acceso a energía eléctrica
+
+# Seguridad Personal
+## Tasa de robos y asaltos
+## Tasa de homicidios
+## Muertes en carreteras
+
+# Fundamentos del Bienestar ----
+
+# Acceso a conocimientos básicos
+## Analfabetismo
+## Matriculación en educación primaria
+## Matriculación en educación secundaria
+## Rezago escolar
+## Paridad de género en educación secundaria
+
+# Acceso a Información y Comunicaciones
+## Usuarios de telefonía movil
+## Usuarios de computadoras
+## Usuarios de internet
+## Cobertura telefonía movil 3G
+
+# Salud y Bienestar
+## Esperanza de vida al nacer
+## Tasa de muertes por IRA
+## Tasa de suicidio
+## Tasa de obesidad
+## Tasa de mortalidad por enfermedades no contagiosas
+
+# Sustentabilidad del Ecosistema
+## Desastres naturales
+## Separación de residuos
+## Cobertura forestal
+## Disponibilidad de agua por persona
+
+
+# Oportunidades ----
+
+# Derechos Personales
+## Libertad de acceso a la información
+## Participación política
+## Derecho a la propiedad privada
+## Libertad de asociación
+
+# Libertad Personal y de Elección
+## Porcentaje de madres adolescentes
+## Población jóvenes Nini
+## Rendición de cuentas
+## Acceso a la cultura y recreación
+
+# Tolerancia e Inclusión
+## Inclusión al adulto mayor
+## Inclusión a población con discapacidad
+## Inclusión población indígena
+## Inclusión de migrantes
+## Inclusión de hogares con jefatura femenina
+
+# Acceso a Educación Superior
+## Población con educación superior
+## Escolaridad promedio de las mujeres
+## Población con educación técnica
